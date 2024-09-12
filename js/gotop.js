bdd7a7d9f24f2a72b62995a265ce64f8b1121ea2
LEEJUNGUEN <jungeun623@gmail.com>
2024-09-12 16:10:54 +0900

    gotop 버튼 사이즈 수정

--- js/gotop.js
+++ js/gotop.js
@@ -1,4 +1,6 @@
 window.addEventListener("load", function () {
+ 
+  
     // gotop
     AOS.init();
     // gotop바로가기 기능
@@ -10,5 +12,19 @@ window.addEventListener("load", function () {
         behavior: "smooth",
       });
     });
+    // gotop 버튼 윗쪽에서는 숨기기
+    const main = document.querySelector(".main")
+    const goTop = document.querySelector(".gotop")
+    function scrollEvent(event){
+      const STANDARD = 1010;
+      if(window.scrollY > STANDARD){
+        main.classList.add("hide")
+        goTop.classList.add("show")
+      }else {
+        main.classList.remove("hide")
+        goTop.classList.remove("show")
+      }
+    }
+    window.addEventListener("scroll", scrollEvent)
   });
   
