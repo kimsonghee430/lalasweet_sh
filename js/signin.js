087eb4ce1765cc2e0e80df1d97376609cd2fa7eb
kimsonghee430 <bokchi8@gmail.com>
2024-09-19 11:46:51 +0900

    푸터 구조, 반응형 및 회원가입란 생성

---
+++ js/signin.js
@@ -0,0 +1,15 @@
+window.onload = function () {
+  const signinBt = document.querySelector(".signinBt");
+  signinBt.addEventListener("click", function() {
+      alert("회원가입이 완료되었습니다.");
+      alert("쿠폰이 정상적으로 발급되었습니다.");
+
+    // 모달 닫기
+    window.close()
+
+    // 페이지 이동
+    setTimeout(function() {
+        window.location.href = "index.html#event";
+    }, 200); // 2초 후 페이지 이동
+}
+  )};
